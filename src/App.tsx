c57b2699384bb34e2399ee6099c3e79399e5b697
a_kiritani <a.kiritani@skillengine.jp>
2023-11-04 13:37:19 +0900

    App.tsxの修正

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
-import React from 'react';
+import React from 'react'
+import './App.scss'
+import Sidebar from './components/sidebar/Sidebar'
 
-function App() {
+// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+function App () {
   return (
     <div className="App">
+      <Sidebar />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
